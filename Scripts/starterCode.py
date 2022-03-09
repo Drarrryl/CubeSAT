b66d954ec2956acb7ccee1785b9d1eb61ad33c1c
Darryl <lubindarryl@gmail.com>
2022-03-09 10:19:46 -0500

    Added Folder

---
+++ Scripts/starterCode.py
@@ -0,0 +1,9 @@
+# This document inhabits the starter code for the CubeSAT project!
+
+var1 = 10
+var2 = 20
+
+if var1 >= var2:
+    print(var1,"is greater than or equal to",var2)
+else:
+    print(var1,"is not greater than or equal to",var2)
